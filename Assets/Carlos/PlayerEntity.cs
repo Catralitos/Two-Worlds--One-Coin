86db0dbdfc75102fe3f27eb5a919f4450f610820
Carlos Marques <catralitos11@gmail.com>
2022-01-28 19:55:05 +0000

    Criei classe PlayerEntity como singleton

---
+++ Assets/Carlos/PlayerEntity.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerEntity : MonoBehaviour
+    {
+        [HideInInspector] public static PlayerEntity Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Debug.LogWarning("Multiple players present in scene! Destroying...");
+                Destroy(gameObject);
+            }
+
+            //Movement = GetComponent<PlayerMovement>();
+            //Health = GetComponent<PlayerHealth>();
+            //Controller = GetComponent<PlayerControls>();
+            //Combat = GetComponent<PlayerCombat>();
+            //UI = GetComponent<PlayerUI>();
+        }
+    }
+}
