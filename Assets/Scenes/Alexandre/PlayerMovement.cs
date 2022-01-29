be53c5c1a8c2b302a0357ad428d6355472b96f64
Crashim03 <alexteloduarte@gmail.com>
2022-01-29 00:30:16 +0000

    Player movement com input novo

--- Assets/Scenes/Alexandre/PlayerMovement.cs
+++ Assets/Scenes/Alexandre/PlayerMovement.cs
@@ -3,37 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Player
-{
-    public class PlayerMovement : MonoBehaviour
+
+public class PlayerMovement : MonoBehaviour
     {
         
-        public float currentMoveSpeed = 5;
-        
-        public float currentJumpPower = 5;
-        
-        public bool isGrounded = false;
+    public Rigidbody2D theRB;
 
-        void Update()
-        {
-            Jump();
-            if (Input.GetKey(KeyCode.D)){
-                Vector3 movement = new Vector3(1f, 0f, 0f);
-                transform.position += movement * Time.deltaTime * currentMoveSpeed;
-                }
-            if (Input.GetKey(KeyCode.A)){
-                Vector3 movement = new Vector3(-1f, 0f, 0f);
-                transform.position += movement * Time.deltaTime * currentMoveSpeed;
-                }
-            
-        }
-
-        void Jump()
-        {   if (Input.GetButtonDown("Jump") && isGrounded == true){
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, currentJumpPower), ForceMode2D.Impulse);
-        }
-        }
+    public float currentMoveSpeed = 5;
         
+    public float currentJumpPower = 5;
+        
+    public bool isGrounded = false;
 
+    private float inputX;
+
+    void Update()
+    {
+        theRB.velocity = new Vector2(inputX * currentMoveSpeed, theRB.velocity.y);
+
+        
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        inputX = context.ReadValue<Vector2>().x;
+    }
+        
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded){
+            theRB.velocity = new Vector2(theRB.velocity.x, currentJumpPower);
+        }
     }
 }
+
