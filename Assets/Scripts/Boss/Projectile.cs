8d89eb6d2e51c45f523093d132dca62837f6e136
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2022-01-30 12:46:40 +0000

    Push ao que tenho

--- Assets/Scripts/Boss/Projectile.cs
+++ Assets/Scripts/Boss/Projectile.cs
@@ -14,17 +14,22 @@ namespace Boss{
         // Start is called before the first frame update
         private void Start()
         {
-            if (_movementVector.x > 0)
-            {
-                this.GetComponent<SpriteRenderer>().flipX = true;
-                this.transform.Rotate(0.0f, 0.0f, 38.0f);
-            } else {
-                this.transform.Rotate(0.0f, 0.0f, -38.0f);
-            }
+           
             Vector3 target = PlayerEntity.Instance.gameObject.transform.position;
             _movementVector = (target - transform.position).normalized;
 
-           
+            if (_movementVector.x > 0)
+            {
+                this.transform.Rotate(0.0f, 0.0f, 38.0f);
+                //this.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+            else
+            {
+                this.transform.Rotate(0.0f, 0.0f, -218.0f);
+                // this.GetComponent<SpriteRenderer>().flipX = true;
+
+            }
         }
 
         // Update is called once per frame
