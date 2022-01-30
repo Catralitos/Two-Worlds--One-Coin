83d3b561291399101398d5ec86ba83d15af48895
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2022-01-30 11:54:48 +0000

    Updated Projectile, Changed Invulnerability Frames

--- Assets/Scripts/Boss/Projectile.cs
+++ Assets/Scripts/Boss/Projectile.cs
@@ -14,8 +14,17 @@ namespace Boss{
         // Start is called before the first frame update
         private void Start()
         {
+            if (_movementVector.x > 0)
+            {
+                this.GetComponent<SpriteRenderer>().flipX = true;
+                this.transform.Rotate(0.0f, 0.0f, 38.0f);
+            } else {
+                this.transform.Rotate(0.0f, 0.0f, -38.0f);
+            }
             Vector3 target = PlayerEntity.Instance.gameObject.transform.position;
             _movementVector = (target - transform.position).normalized;
+
+           
         }
 
         // Update is called once per frame
